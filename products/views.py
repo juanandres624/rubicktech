78da2559f1b747eca6a9ba91f559287455373949
juanandres624 <juanandres624@hotmail.com>
2022-12-22 11:55:35 -0500

    import errores

--- products/views.py
+++ products/views.py
@@ -1,6 +1,3 @@
-from ast import For
-from dataclasses import field
-from turtle import textinput
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProductsForm,VariationForm,ImageForm,CatalogForm
